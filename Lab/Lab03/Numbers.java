7fe8c76db220b28b468a9386033b044cfcbdbfcb
Rishi Sanjaykumar Patel <64521819+patelrishi2308@users.noreply.github.com>
2024-09-19 17:23:29 -0400

    Update Numbers.java

--- Lab/Lab03/Numbers.java
+++ Lab/Lab03/Numbers.java
@@ -1,20 +1,27 @@
 public class Numbers {
   static int nextAvailableNumber = 6;
-  int id = 0;
+  int id = 0; // instance variable
   private Numbers() {
+    try {
+      Thread.sleep(1000);
+    } catch (Exception e) {
+
+    }
     this.id = Numbers.nextAvailableNumber++;
   }
   public static Numbers singleton = null;
-  public static Numbers makeInstance() {
+  public synchronized static Numbers makeInstance() { // al
     if (Numbers.singleton == null)
       Numbers.singleton = new Numbers();
     return Numbers.singleton;
   }
   public String toString() {
-    return "I am number: " + this.id;
+    return "I am number " + this.id;
   }
   public static void main(String[] args) {
     One a = new One();
     a.start();
+    (new Two()).start();
+    (new Three()).start();
   }
 }
