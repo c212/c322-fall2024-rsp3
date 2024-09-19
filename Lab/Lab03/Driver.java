d6968da5073abd5eb5b62f92f5923e892f415393
Rishi Sanjaykumar Patel <64521819+patelrishi2308@users.noreply.github.com>
2024-09-19 17:02:43 -0400

    Create Driver.java

---
+++ Lab/Lab03/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+  public static void main(String[] args) {
+    Numbers a = new Numbers();
+    Numbers b = new Numbers();
+    Numbers c = new Numbers();
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
