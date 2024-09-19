a2b3488b72ce514557d0417fc3b531b14969c1b5
Rishi Sanjaykumar Patel <64521819+patelrishi2308@users.noreply.github.com>
2024-09-19 17:17:30 -0400

    Create One.java

---
+++ Lab/Lab03/One.java
@@ -0,0 +1,14 @@
+class One extends Thread {
+  public void run() {
+    try {
+      while (true) {
+        Numbers a = Numbers.makeInstance();
+        int wait = (int) (Math.random() * 400 + 100);
+        System.out.println("One will wait: " + wait + " " + a);
+        sleep(wait);
+      }
+    } catch (InterruptedException  e) {
+      return;
+    }
+  }
+}
