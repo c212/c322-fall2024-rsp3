c97b85a05e44abf69d9069837a569a21374cddf2
Rishi Sanjaykumar Patel <64521819+patelrishi2308@users.noreply.github.com>
2024-09-19 17:13:06 -0400

    Update Numbers.java

--- Lab/Lab03/Numbers.java
+++ Lab/Lab03/Numbers.java
@@ -2,12 +2,15 @@ public class Numbers {
   static int nextAvailableNumber = 6;
   int id = 0;
   private Numbers() {
-
+    this.id = Numbers.nextAvailableNumber++;
   }
   public static Numbers singleton = null;
   public static Numbers makeInstance() {
     if (Numbers.singleton == null)
       Numbers.singleton = new Numbers();
     return Numbers.singleton;
+  }
+  public String toString() {
+    return "I am number: " + this.id;
   }
 }
